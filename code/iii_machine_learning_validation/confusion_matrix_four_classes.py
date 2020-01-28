55d38c06c73bda80d4b47d480faa034dee22a4c8
Thomas Nield <thomasnield@live.com>
2020-01-28 11:11:06 -0600

    start adding confusion matrix

---
+++ code/iii_machine_learning_validation/confusion_matrix_four_classes.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import plot_confusion_matrix
+
+data = pd.read_csv("https://tinyurl.com/y6r7qjrp")
+
+# grab independent variable column
+inputs = data.iloc[:, :-1]
+
+# grab dependent variable column
+output = data.iloc[:, -1]
+
+# build logistic regression, note CVLogisticRegression is also recommended to use cross-validation
+fit = LogisticRegression().fit(inputs, output)
+
+# Plot confusion matrix with and without normalization
+titles_options = [("Confusion Matrix", None),
+                  ("Normalized Confusion Matrix", 'true')]
+
+for title, normalize in titles_options:
+    disp = plot_confusion_matrix(fit, inputs, output,
+                                 cmap=plt.cm.Blues,
+                                 normalize=normalize)
+    disp.ax_.set_title(title)
+
+    print(title)
+    print(disp.confusion_matrix)
+
+plt.show()
